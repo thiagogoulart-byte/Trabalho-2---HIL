57b707f88a0a02d9d115057ec509f5ad362b186e
Thiago <thiago.goulart@estudante.ufjf.br>
2026-06-30 15:15:31 -0300

    Funcionando

--- cla_example/main.c
+++ cla_example/main.c
@@ -10,6 +10,8 @@
 
 #define L 3e-3f
 #define PI 3.1415f
+#define FS = 20000.0f     // 
+#define FREQ 60.0f   
 
 // termos do modelo do conversor
 float XR = 30.0f;
@@ -23,7 +25,12 @@ float vinv, iL1, u1;
 float iL0 = 0.0f;
 float u0 = 0.0f;
 float Vdc = 400.0f;
-// float vg = 220.0f;
+
+#pragma DATA_SECTION(iL, "CpuToCla1MsgRAM");
+volatile float iL;
+
+uint16_t DAC_iL;
+float teste_iL_dac;
 
 // Chaves
 volatile bool S1;
@@ -32,68 +39,36 @@ volatile bool S3;
 volatile bool S4;
 
 
+// Variáveis para calcular a tensão da rede
+float ang;
+volatile float vg = 0;
+float vg_ant;
+// Compartilhar para usar o no Feedforward do controlador
+#pragma DATA_SECTION(vg, "CpuToCla1MsgRAM");
+#pragma DATA_SECTION(vg_ant, "CpuToCla1MsgRAM");
+
+
+#pragma DATA_SECTION(ePwm_TimeBaseA,"CpuToCla1MsgRAM");
 uint32_t ePwm_TimeBaseA;
+#pragma DATA_SECTION(ePwm_TimeBaseB,"CpuToCla1MsgRAM");
 uint32_t ePwm_TimeBaseB;
 
 
-// float amplitude = 13.0f;
-float fs = 20000.0f;     // 
-#define freq 60.0f     //
-// #define fsck 15e5f
-
-
-#pragma DATA_SECTION(vg, "CpuToCla1MsgRAM");
-volatile float vg = 0;
-
-#pragma DATA_SECTION(AMP, "CpuToCla1MsgRAM");
-volatile float AMP = 13.0f;
-
-#pragma DATA_SECTION(iL, "CpuToCla1MsgRAM");
-float iL;
-
-#pragma DATA_SECTION(u, "Cla1ToCpuMsgRAM");
-float u;
-
-#pragma DATA_SECTION(fVal,"CpuToCla1MsgRAM");
-float fVal;
-#pragma DATA_SECTION(fResult,"Cla1ToCpuMsgRAM");
-float fResult;
-
-#pragma DATA_SECTION(ePwm_TimeBaseA,"CpuToCla1MsgRAM");
-#pragma DATA_SECTION(ePwm_TimeBaseB,"CpuToCla1MsgRAM");
-
-#pragma DATA_SECTION(fResult,"Cla1ToCpuMsgRAM");
-extern uint32_t CAMPA; 
-
-uint16_t DAC_iL;
-
-float ang;
-
+// Variáveis para buffer
 #define TAM_BUFFER 1000
+uint16_t idx_buffer = 0;
 
+// Buffers
 float buffer_iL[TAM_BUFFER];
-float buffer_vinv[TAM_BUFFER];
-float buffer_u[TAM_BUFFER];
-
+// float buffer_vinv[TAM_BUFFER];
+// float buffer_u[TAM_BUFFER];
 // float buffer_pwm1A[TAM_BUFFER];  
 // float buffer_pwm1B[TAM_BUFFER];  
 // float buffer_pwm3A[TAM_BUFFER]; 
 // float buffer_pwm3B[TAM_BUFFER];
 
-uint16_t idx_buffer = 0;
-
+// Variável para rodar a planta
 volatile bool g_new_step_ready = true;
-
-uint32_t PULAR_PASSO = 1;
-uint32_t contar_passos = 0;
-uint32_t temp_delay = 0;
-extern float theta;
-#pragma DATA_SECTION(vg_ant, "CpuToCla1MsgRAM");
-float vg_ant;
-float di_dt;
-float teste_iL_dac;
-float a;
-float b;
 
 void main(void)
 {
@@ -105,12 +80,11 @@ void main(void)
     ePwm_TimeBaseA = EPWM_getTimeBasePeriod(myEPWM0_BASE);
     ePwm_TimeBaseB = EPWM_getTimeBasePeriod(myEPWM1_BASE);
 
-    Ts = 1.0f / (20.0e5f);
-    R = 2.0f*PI*freq*L/XR;
-    float den = (2.0f * L / Ts) + R;
-
+    Ts = 1.0f / (10.0e5f);
+    R = 2.0f*PI*FREQ*L/XR;
 
     // Constantes do modelo
+    float den = (2.0f * L / Ts) + R;
     a11 = ((2.0f * L / Ts) - R) / den;
     b00 = 1.0f / den;
     b11 = b00;
@@ -120,11 +94,10 @@ void main(void)
 
     for(;;)
     {
-
         if (g_new_step_ready)
         {
             g_new_step_ready = false;
-
+            // Calcular a tensão da rede
             ang += 2.0f * 3.1415f * 60.0 * Ts; 
             if(ang >= 6.283185f)  ang -= 6.283185f;     
             vg = 311.1269f*sinf(ang); // Tensão da rede
@@ -133,12 +106,14 @@ void main(void)
             if (S1 == 1 && S4 == 1)       vinv = Vdc; 
             else if (S2 == 1 && S3 == 1)  vinv = -Vdc; 
             else                          vinv = 0.0f;
-
+            // Planta
             u0 = vinv - vg_ant;
         
             iL = a11*iL0 + b00*u0 + b11*u1;
 
+            // Buffers
             buffer_iL[idx_buffer] = iL;
+            //// Colocando os buffers na mesma escala
             // buffer_u[idx_buffer] = u*10.0f;
             // buffer_vinv[idx_buffer] = vinv*0.05f;
             // buffer_pwm1A[idx_buffer] = S1 * 10.0f;
@@ -163,10 +138,8 @@ void main(void)
     }
 }
 
-
 __interrupt void cla1Isr1 ()
 {
-    fVal = fResult;
     Interrupt_clearACKGroup(INT_myCLA01_INTERRUPT_ACK_GROUP);
 }
 
@@ -188,7 +161,6 @@ __interrupt void INT_GPIO_S2_XINT_ISR(void)
     S2 = GPIO_readPin(GPIO_S2);   
     Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP12);
 }
-
 
 __interrupt void INT_GPIO_S3_XINT_ISR(void)
 {
--- cla_example/shared_vars.h
+++ cla_example/shared_vars.h
@@ -8,18 +8,10 @@
 #ifndef SHARED_VARS_H_
 #define SHARED_VARS_H_
 
-extern float fVal;
-extern float fResult;
-extern float u;
 extern float iL;
 extern float vg;
-extern float AMP;
+extern float vg_ant;
 extern uint32_t ePwm_TimeBaseA;
 extern uint32_t ePwm_TimeBaseB;
-// extern float Iref;
-extern float theta;
-
-
-// extern float a1, a2, b0, b1, b2, ki, kp;
 
 #endif /* SHARED_VARS_H_ */
